cb4224f7268336fd5946324d3c7ac6220bc39c4c
Adam <asur0008@dyn-49-127-9-24.its.monash.edu.au>
2022-05-30 12:34:40 +1000

    30 May

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,32 +1,107 @@
 import random
+import sys
 
-def game_play(choice1,choice2, row_1, row_2, row_3, title):
+
+def score_check(row1, row2, row3):
+
+    if row1.count("O") == 3 or row2.count("O") == 3 or row3.count("O") == 3:
+        print("Player with circles won!")
+        sys.exit()
+    elif row1[1] == row2[1] == row3[1] == 'O' or row1[2] == row2[2] == row3[2] == 'O' or row1[3] == row2[3] == row3[3] == 'O' or row1[1] == row2[2] == row3[3] == 'O' :
+        print("Player with circles won!")
+        sys.exit()
+    elif row1.count("X") == 3 or row2.count("X") == 3 or row3.count("X") == 3:
+        print("Player with crosses won!")
+        sys.exit()
+    elif row1[1] == row2[1] == row3[1] == 'X' or row1[2] == row2[2] == row3[2] == 'X' or row1[3] == row2[3] == row3[3] == 'X' or row1[1] == row2[2] == row3[3] == 'X':
+        print("Player with crosses won!")
+        sys.exit()
+
+def game_play1(choice1, row_1, row_2, row_3, title, played_counter):
     if choice1 == 'A':
         row_1.pop(1)
-        row_1.insert(1,'O')
+        if played_counter % 2 == 0:
+            row_1.insert(1,'X')
+        else:
+            row_1.insert(1, 'O')
         print(*title, '\n', row_1, '\n', row_2, '\n', row_3)
     if choice1 == 'B':
         row_1.pop(2)
-        row_1.insert(2,'O')
+        if played_counter % 2 == 0:
+            row_1.insert(2,'X')
+        else:
+            row_1.insert(2, 'O')
         print(*title, '\n', row_1, '\n', row_2, '\n', row_3)
     if choice1 == 'C':
-        row_1.pop(2)
-        row_1.insert(2,'O')
+        row_1.pop(3)
+        if played_counter % 2 == 0:
+            row_1.insert(3,'X')
+        else:
+            row_1.insert(3, 'O')
         print(*title, '\n', row_1, '\n', row_2, '\n', row_3)
-
+    if choice1 == 'D':
+        row_2.pop(1)
+        if played_counter % 2 == 0:
+            row_2.insert(1,'X')
+        else:
+            row_2.insert(1, 'O')
+        print(*title, '\n', row_1, '\n', row_2, '\n', row_3)
+    if choice1 == 'E':
+        row_2.pop(2)
+        if played_counter % 2 == 0:
+            row_2.insert(2,'X')
+        else:
+            row_2.insert(2, 'O')
+        print(*title, '\n', row_1, '\n', row_2, '\n', row_3)
+    if choice1 == 'F':
+        row_2.pop(3)
+        if played_counter % 2 == 0:
+            row_2.insert(3,'X')
+        else:
+            row_2.insert(3, 'O')
+        print(*title, '\n', row_1, '\n', row_2, '\n', row_3)
+    if choice1 == 'G':
+        row_3.pop(1)
+        if played_counter % 2 == 0:
+            row_3.insert(1, 'X')
+        else:
+            row_3.insert(1, 'O')
+        print(*title, '\n', row_1, '\n', row_2, '\n', row_3)
+    if choice1 == 'H':
+        row_3.pop(2)
+        if played_counter % 2 == 0:
+            row_3.insert(2,'X')
+        else:
+            row_3.insert(2, 'O')
+        print(*title, '\n', row_1, '\n', row_2, '\n', row_3)
+    if choice1 == 'I':
+        row_3.pop(3)
+        if played_counter % 2 == 0:
+            row_3.insert(3,'X')
+        else:
+            row_3.insert(3, 'O')
+        print(*title, '\n', row_1, '\n', row_2, '\n', row_3)
+# def game_play2(choice2, row_1, row_2, row_3, title):
+    return row_1, row_2, row_3
 
 def main():
 
     player1, player2 = input("Enter two names use space: ").split()
     print("O is {} and X is {}".format(player1, player2))
     print('The board looks as follows:')
+    played_counter=0
     title = ['        C1', '    C2', '   C3']
     row_1 = ['R1', 'A', 'B', 'C']
     row_2 = ['R2', 'D', 'E', 'F']
     row_3 = ['R3', 'G', 'H', 'I']
     print(*title, '\n', row_1, '\n', row_2, '\n', row_3)
-
-    choice_1 = input('O type position: ')
-    choice_2 = input('O type position: ')
-    game_play(choice_1,choice_2, row_1, row_2, row_3, title)
+    while True:
+        choice_1 = input(f'Player {player1} "O" circle - type position: ')
+        played_counter += 1
+        game_play1(choice_1, row_1, row_2, row_3, title, played_counter)
+        choice_2 = input(f'Player {player2} "X" cross - type position: ')
+        played_counter += 1
+        game_play1(choice_2, row_1, row_2, row_3, title, played_counter)
+        # game_play2(choice_1, row_1, row_2, row_3, title)
+        score_check(row_1,row_2,row_3)
 main()
